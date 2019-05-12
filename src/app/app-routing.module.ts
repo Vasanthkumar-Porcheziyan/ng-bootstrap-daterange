71b7dc8afa516577b68c8fa036bf60115b75a823
VasanthPorcheziyan <vasanthporcheziyan@gmail.com>
2019-05-12 12:59:38 +0530

    default route to load date-range-picker

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {BootstrapDateComponent} from './bootstrap-date/bootstrap-date.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BootstrapDateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
